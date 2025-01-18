8e879d65810c7a5cbd442cf220e18ed5e9151578
Rustambx <rustambx@gmail.com>
2025-01-19 00:19:25 +0500

    Fixed KafkaQueue pop method

--- src/KafkaQueue.php
+++ src/KafkaQueue.php
@@ -40,7 +40,7 @@ class KafkaQueue extends Queue implements QueueContract
 
     public function pop($queue = null)
     {
-        $this->consumer->subscribe($queue);
+        $this->consumer->subscribe([$queue]);
 
         try {
             $message = $this->consumer->consume(120 * 1000);
